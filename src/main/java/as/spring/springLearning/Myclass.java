cc582a604f3755def1a7e7cbfaec7df9df5b9629
Ashish Singh <theashish752@gmail.com>
2025-12-08 20:37:30 +0530

    Create spring API

---
+++ src/main/java/as/spring/springLearning/Myclass.java
@@ -0,0 +1,12 @@
+package as.spring.springLearning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Myclass {
+    @GetMapping("hello")
+    public String sayHello(){
+       return "Hello Ashish";
+    }
+}
